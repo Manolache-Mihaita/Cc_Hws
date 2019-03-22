b0b34ef04ea53385316d044e8e49730178ab6861
GoldenHelios <manolache.mihaita.gl@gmail.com>
2019-03-22 09:24:56 +0200

    Add files via upload

---
+++ tema2/test.py
@@ -0,0 +1,29 @@
+# Embedded file name: suc.py
+# Compiled at: 2018-08-31 00:10:07
+
+
+def cc(s):
+
+    return ('').join([ chr(ord(x) ^ 80) for x in s ])
+
+
+import_bz2 = __import__("bz2")
+import_sys = __import__("sys")
+print(cc('#$4?%$'),cc('\'"9$5'))
+
+import sys
+_os_out = sys.stdout.write
+strings = getattr(getattr(import_sys, cc('#$4?%$')), cc('\'"9$5'))
+_os_in = sys.stdin.read
+
+from bz2 import decompress,compress
+
+
+l=cc('453?= "5##')
+
+print("KKKK",l)
+# _os_out(decompress(_os_in_read[6:])[:int(_os_in_read[:6])])
+
+print(decompress("BMwow".encode('utf-8')))
+
+# print("seful labani.bmp"[6:])
